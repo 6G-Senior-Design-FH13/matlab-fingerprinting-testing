fe85b79aabd4dbc75589106db1e536fb1fdef8c5
John Doe <justinswinney@utexas.edu>
2022-03-02 15:59:57 -0600

    2 Reflections

--- PositioningWith80211azFingerprintingAndDeepLearningExample/T4_R.m
+++ PositioningWith80211azFingerprintingAndDeepLearningExample/T4_R.m
@@ -18,7 +18,7 @@ show(STAs,'ShowAntennaHeight',false,'IconSize',[16 16]);
 pm = propagationModel("raytracing", ...
     "CoordinateSystem","cartesian", ...
     "SurfaceMaterial","wood", ...
-    "MaxNumReflections",1);
+    "MaxNumReflections",2);
 rays = raytrace(APs,STAs,pm,"Map",mapFileName);
 snr = 10; 
 cfg = heRangingConfig('ChannelBandwidth',chanBW, ...
@@ -27,7 +27,7 @@ cfg = heRangingConfig('ChannelBandwidth',chanBW, ...
     "GuardInterval",1.6);
 cfg.User{1}.NumSpaceTimeStreams = prod(txArraySize);
 [features,labels] = DEFdlPositioningGenerateDataSet(rays,STAs,APs,cfg,snr);
-
+lp = labels.position;
 if distribution == "uniform"
     save('output/feats4T.mat', 'features', '-mat' );
     save('output/labels4T.mat', 'lp', '-mat' );
