89e39a41e18bca2bd902f1cb1ad1a9d6a814239a
John Doe <justinswinney@utexas.edu>
2022-03-23 14:31:15 -0500

    Waveform Generation Default File

--- PositioningWith80211azFingerprintingAndDeepLearningExample/T4_R.m
+++ PositioningWith80211azFingerprintingAndDeepLearningExample/T4_R.m
@@ -1,11 +1,10 @@
-for i=1:10
     mapFileName = "office.stl";
     %viewer = siteviewer("SceneModel",mapFileName,"Transparency",0.25);
     distribution = "uniform";
     txArraySize = [4 1]; % Linear transmit array
     rxArraySize = [1 1]; % Linear receive array
     chanBW = "CBW40"; 
-    staSeparation = .1; % STA separation, in meters, used only when the distribution is uniform
+    staSeparation = .5; % STA separation, in meters, used only when the distribution is uniform
     numSTAs = 300;  
     S = RandStream("mt19937ar","Seed",5489); % Set the RNG for reproducibility.
     RandStream.setGlobalStream(S);
@@ -29,14 +28,14 @@ for i=1:10
     cfg.User{1}.NumSpaceTimeStreams = prod(txArraySize);
     [features,labels] = DEFdlPositioningGenerateDataSet(rays,STAs,APs,cfg,snr);
     lp = labels.position;
-    nameF=['output/feats4T(' int2str(i) ').mat'];
+    %nameF=['output/feats4T(' int2str(i) ').mat'];
     if distribution == "uniform"
         %save(name, 'features', '-mat' );
         %save('output/labels4T.mat', 'lp', '-mat' );
-        save('output/feats4T_.1R.mat', 'features', '-mat' );
-        save('output/labels4T_.1R.mat', 'lp', '-mat' );
+        save('output/feats4T_.5R.mat', 'features', '-mat' );
+        save('output/labels4T_.5R.mat', 'lp', '-mat' );
     else
         save('output/feats4T_RValidate.mat', 'features', '-mat' );
         save('output/labels4T_RValidate.mat', 'lp', '-mat' );
     end
-end
+
