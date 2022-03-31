ad9c379f200c03ce02c9e53efaacd8650590cccb
Kaushik Gagan (gk6649) <gk6649@ece-a51990.austin.utexas.edu>
2022-03-31 15:32:04 -0500

    working dgx simulation script

--- PositioningWith80211azFingerprintingAndDeepLearningExample/dgxrun.sh
+++ PositioningWith80211azFingerprintingAndDeepLearningExample/dgxrun.sh
@@ -1,5 +1,6 @@
 # to run:
-# source dgxrun.sh &
+# source dgxrun.sh
 
-nice -n 5 matlab -nodisplay -nojvm -nosplash -nodesktop -r "try, run('main.m'), catch, exit(1), end, exit(0);" > output.txt
-echo "matlab exit code: $?"
+# matlab -nodisplay -nojvm -nosplash -nodesktop -r "run('main.m');exit;" > output.txt
+
+matlab -batch "main"
