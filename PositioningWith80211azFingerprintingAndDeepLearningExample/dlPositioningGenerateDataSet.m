112e73b4777a913f7b51280126e5ef8b864cd222
JeanLee <61173463+jl67227@users.noreply.github.com>
2022-03-30 21:09:45 -0500

    Add files via upload
    chirp update

--- PositioningWith80211azFingerprintingAndDeepLearningExample/dlPositioningGenerateDataSet.m
+++ PositioningWith80211azFingerprintingAndDeepLearningExample/dlPositioningGenerateDataSet.m
@@ -204,7 +204,7 @@
 
 
 
-function [range] = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
+function P = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
     % Adjust power of noise added such that the SNR is per active
     % subcarrier
     % snrAdj = snr-10*log10();
@@ -212,9 +212,11 @@ function [range] = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
 
 % % % % % % % % % % % % % % % % %     
     sr = 200e6;
-    x = [];
-    y = [];
-    z = [];
+%     x = [];
+%     y = [];
+%     z = [];
+    samples = [];
+    labels.position = zeros([3 numChan]);
     for i = 1:numChan
         txn = mod(i-1,height(rays))+1;
         rxn = ceil(i/height(rays));
@@ -227,18 +229,24 @@ function [range] = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
         peField = chirp(t,0,t(end),100e6)'; % 5e8 = sr/2
 % % % % % % % % % %         
         txWaveform = peField;
-        figure(1);
-        pspectrum(txWaveform,sr,'spectrogram', 'TimeResolution',0.0000001, 'OverlapPercent',99);
+%         figure(1);
+%         pspectrum(txWaveform,sr,'spectrogram', 'TimeResolution',0.0000001, 'OverlapPercent',99);
+        if ~isempty(rays{i})
+            rtChan = comm.RayTracingChannel(rays{i},APs(txn),STAs(rxn)); % Create channel
+    
+            rtChan.SampleRate = sr;
+            rtChan.ReceiverVirtualVelocity = [0; 0; 0]; % Stationary Receiver
+            rtChan.NormalizeChannelOutputs = false;
+            
+            rxChan = rtChan(txWaveform); % Pass waveform through channel
+%             labels(i,1) = STAs(1).AntennaPosition(1,1);
+%             labels(i,2) = STAs(1).AntennaPosition(2,1);
+%             labels(i,3) = STAs(1).AntennaPosition(3,1);
+            labels.position(:,i) = [STAs(rxn).AntennaPosition];
+            labels.class(i) = categorical(cellstr(STAs(rxn).Name)); 
 
-
-        rtChan = comm.RayTracingChannel(rays{i},APs(txn),STAs(rxn)); % Create channel
-        rtChan.SampleRate = sr;
-        rtChan.ReceiverVirtualVelocity = [0; 0; 0]; % Stationary Receiver
-        rtChan.NormalizeChannelOutputs = false;
-        
-        rxChan = rtChan(txWaveform); % Pass waveform through channel
-        figure(2);
-        pspectrum(rxChan,sr,'spectrogram', 'TimeResolution',0.0000001, 'OverlapPercent',99);
+%         figure(2);
+%         pspectrum(rxChan,sr,'spectrogram', 'TimeResolution',0.0000001, 'OverlapPercent',99);
         % Add noise to the the received waveform for each snr value,
         % Perform synchronization, channel estimation
         for j=1:length(snrs)
@@ -247,35 +255,45 @@ function [range] = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
 %             if isempty(chanEst)
 %                 continue % Synchronization fails
 %             end
-            d = finddelay(rx,txWaveform);
-            display(d);
+%             d = finddelay(rx,txWaveform);
+%             display(d);
 
             L = 160; %length of signal
             r = xcorr(rx, peField);
-            Fs = sr;
-            Y = fft(r);
-            P2 = abs(Y/L);
-            P1 = P2(1:L/2+1);
-            P1(2:end-1) = 2*P1(2:end-1);
-            f = Fs*(0:(L/2))/L;
-            figure(11);
-            plot(f,P1); %single sided spectrum
-            hold on;
-            bw = 100e6; %CBW40 = 40MHz
-            T = length(t); %time duration of the chirp
-            %find the freq of the peak
-            [maxY, indexMaxY] = max(P1);
-            fp = f(indexMaxY(1));
-            %find the range
-            range_ = bw/(T*fp);
-            disp(range_);
+%             figure();
+            [P,F,T] = pspectrum(r,sr,'spectrogram', 'TimeResolution',0.0000001, 'OverlapPercent',99);
+            samples(:,:,i) = P;
+%             axis off
+%             colorbar off;
+%             set(gca,'XColor', 'none','YColor','none', 'DefaultFigureRenderer', 'opengl');
+%             title '';
+%             name = '.png';
+%             fullname= [ num2str(i,'%d') name];
+%             saveas(gcf,fullname);
+%             Fs = sr;
+%             Y = fft(r);
+%             P2 = abs(Y/L);
+%             P1 = P2(1:L/2+1);
+%             P1(2:end-1) = 2*P1(2:end-1);
+%             f = Fs*(0:(L/2))/L;
+%             figure(11);
+%             plot(f,P1); %single sided spectrum
+%             hold on;
+%             bw = 100e6; %CBW40 = 40MHz
+%             T = length(t); %time duration of the chirp
+%             %find the freq of the peak
+%             [maxY, indexMaxY] = max(P1);
+%             fp = f(indexMaxY(1));
+%             %find the range
+%             range_ = bw/(T*fp);
+%             disp(range_);
         end
-        range(i) = range_;
-%         disp(rays{i}.TransmitterLocation);
-        x(i) = rays{i}(1,1).TransmitterLocation(1);
-        y(i) = rays{i}(1,1).TransmitterLocation(2);
-        z(i) = rays{i}(1,1).TransmitterLocation(3);
-    end 
+%         range(i) = range_;
+% %         disp(rays{i}.TransmitterLocation);
+%         x(i) = rays{i}(1,1).TransmitterLocation(1);
+%         y(i) = rays{i}(1,1).TransmitterLocation(2);
+%         z(i) = rays{i}(1,1).TransmitterLocation(3);
+        end 
     % fix the equation.....
 %     A = [2*(APs(2).AntennaPosition(1,1) - APs(1).AntennaPosition(1,1)) 2*(APs(2).AntennaPosition(2,1) - APs(1).AntennaPosition(2,1)) 2*(APs(2).AntennaPosition(3,1) - APs(1).AntennaPosition(3,1));
 %         2*(APs(3).AntennaPosition(1,1) - APs(1).AntennaPosition(1,1)) 2*(APs(3).AntennaPosition(2,1) - APs(1).AntennaPosition(2,1)) 2*(APs(3).AntennaPosition(3,1) - APs(1).AntennaPosition(3,1))];
@@ -290,14 +308,25 @@ function [range] = dlPositioningGenerateDataSet(rays, STAs, APs, cfg, snrs)
 %     b = [((range(1)^2 - range(4)^2)-(APs(1).AntennaPosition(1,1)^2 - APs(4).AntennaPosition(1,1)^2)-(APs(1).AntennaPosition(2,1)^2 - APs(4).AntennaPosition(2,1)^2)-(APs(1).AntennaPosition(3,1)^2 - APs(4).AntennaPosition(3,1)^2));
 %         ((range(2)^2 - range(4)^2)-(APs(2).AntennaPosition(1,1)^2 - APs(4).AntennaPosition(1,1)^2)-(APs(2).AntennaPosition(2,1)^2 - APs(4).AntennaPosition(2,1)^2)-(APs(2).AntennaPosition(3,1)^2 - APs(4).AntennaPosition(3,1)^2));
 %         ((range(3)^2 - range(4)^2)-(APs(3).AntennaPosition(1,1)^2 - APs(4).AntennaPosition(1,1)^2)-(APs(3).AntennaPosition(2,1)^2 - APs(4).AntennaPosition(2,1)^2)-(APs(3).AntennaPosition(3,1)^2 - APs(4).AntennaPosition(3,1)^2))];
-    A = [2*(x(4) - x(1)) 2*(y(4) - y(1)) 2*(z(4) - z(1));
-        2*(x(4) - x(2)) 2*(y(4) - y(2)) 2*(z(4) - z(2));
-        2*(x(4) - x(3)) 2*(y(4) - y(3)) 2*(z(4) - z(3))];
-    b = [((range(1)^2 - range(4)^2)-(x(1)^2 - x(4)^2)-(y(1)^2 - y(4)^2)-(z(1)^2 - z(4)^2));
-        ((range(2)^2 - range(4)^2)-(x(2)^2 - x(4)^2)-(y(2)^2 - y(4)^2)-(z(2)^2 - z(4)^2));
-        ((range(3)^2 - range(4)^2)-(x(3)^2 - x(4)^2)-(y(3)^2 - y(4)^2)-(z(3)^2 - z(4)^2))];
-    
-    position = pinv(A)*b;
-    disp(position);
-    disp(STAs.AntennaPosition);
-end
+%     A = [2*(x(4) - x(1)) 2*(y(4) - y(1)) 2*(z(4) - z(1));
+%         2*(x(4) - x(2)) 2*(y(4) - y(2)) 2*(z(4) - z(2));
+%         2*(x(4) - x(3)) 2*(y(4) - y(3)) 2*(z(4) - z(3))];
+%     b = [((range(1)^2 - range(4)^2)-(x(1)^2 - x(4)^2)-(y(1)^2 - y(4)^2)-(z(1)^2 - z(4)^2));
+%         ((range(2)^2 - range(4)^2)-(x(2)^2 - x(4)^2)-(y(2)^2 - y(4)^2)-(z(2)^2 - z(4)^2));
+%         ((range(3)^2 - range(4)^2)-(x(3)^2 - x(4)^2)-(y(3)^2 - y(4)^2)-(z(3)^2 - z(4)^2))];
+%     
+%     position = pinv(A)*b;
+%     disp(position);
+%     disp(STAs.AntennaPosition);
+
+
+    end
+labels.position = labels.position(:, 1:height(rays):end);
+labels.class = labels.class(:, 1:height(rays):end); % Remove duplicated locations
+
+% Create and scale training labels from rx locations to correct size
+labels.position = repelem(labels.position, 1, length(snrs));
+labels.class = repelem(labels.class, 1, length(snrs));
+
+save('samplesChirp.mat','samples','-mat', '-v7.3');
+save('labelsChirp.mat','labels', '-mat', '-v7.3');
