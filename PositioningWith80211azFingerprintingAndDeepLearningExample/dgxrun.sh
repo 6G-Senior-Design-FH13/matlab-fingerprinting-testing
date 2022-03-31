f719b94c5544735b33901df890ca5b9a0f71dba6
Kaushik Gagan (gk6649) <gk6649@ece-a51990.austin.utexas.edu>
2022-03-31 00:34:47 -0500

    fixed script

--- PositioningWith80211azFingerprintingAndDeepLearningExample/dgxrun.sh
+++ PositioningWith80211azFingerprintingAndDeepLearningExample/dgxrun.sh
@@ -1,3 +1,5 @@
-nice -n 5 matlab -nodisplay -nojvm -nosplash -nodesktop -r \
-    "try, run('main.m'), catch, exit(1), end, exit(0);"
+# to run:
+# source dgxrun.sh &
+
+nice -n 5 matlab -nodisplay -nojvm -nosplash -nodesktop -r "try, run('main.m'), catch, exit(1), end, exit(0);" > output.txt
 echo "matlab exit code: $?"
